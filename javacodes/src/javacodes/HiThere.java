542313392888fc1b93f501465e27b60ee427aa9f
myjobsash <myjobsash@gmail.com>
2024-01-29 09:32:39 +0530

    First java program push

---
+++ javacodes/src/javacodes/HiThere.java
@@ -0,0 +1,9 @@
+package javacodes;
+
+public class HiThere {
+
+	public static void main(String[] args) {
+		System.out.println("Hello all Java guys out there......");
+	}
+
+}
---
+++ javacodes/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 5600G
+ *
+ */
+module javacodes {
+}
